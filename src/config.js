3d4b5deb11fb0f67f0d43b424accce1901f52e3c
Loc Nguyen <nguyentongloc1996@gmail.com>
2021-08-16 17:39:41 +0700

    fix

--- src/config.js
+++ src/config.js
@@ -4,7 +4,13 @@ console.log(docElement.clientWidth)
 const width = docElement.clientWidth > '100%' ? '100%' : docElement.clientWidth
 const height = docElement.clientHeight > '100%' ? '100%' : docElement.clientHeight
 export default {
-  width: width,
-  height: height,
-  type: Phaser.AUTO
+  width: width * 2,
+  height: height * 2,
+  type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 }
+    }
+  }
 }
--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,12 +1,35 @@
 import Phaser from 'phaser'
+let Ball
+
 export default class Boot extends Phaser.Scene {
   preload () {
-
+    this.load.image('background', 'assets/images/background.jpg')
   }
 
   create () {
-    console.log(this.game)
-    const text = this.add.text(this.game.config.width / 2, this.game.config.height / 2, 'Loading fonts', { font: '16px Arial', fill: 'white', align: 'center' })
-    text.setOrigin(0.5, 0.5)
+    const gameWidth = this.game.config.width
+    const gameHeight = this.game.config.height
+    this.add.tileSprite(0, 0, gameWidth * 3, gameHeight * 3, 'background')
+
+    Ball = this.add.circle(200, 300, 10, 0xffffff, 1)
+    this.physics.add.existing(Ball)
+    Ball.body.setCollideWorldBounds(true, 1, 1)
+    Ball.body.setVelocity(200, 200)
+    console.log(this.physics)
+    this.cameras.main.width = gameWidth / 2
+    this.cameras.main.height = gameHeight / 2
+    this.cameras.main.startFollow(Ball)
+  }
+
+  update (delta) {
+    const angle = Phaser.Math.Angle.Between(Ball.x, Ball.y, this.input.mousePointer.x, this.input.mousePointer.y)
+    Ball.setRotation(angle + Math.PI / 2)
+    if (this.input.mousePointer) {
+      // this.physics.moveToObject(Ball, this.input, 240);
+      Ball.body.setVelocity(this.input.mousePointer.x - Ball.x, this.input.mousePointer.y - Ball.y)
+      // this.physics.arcade.moveToPointer(Ball, 100)
+      // console.log(this.input.mousePointer.x, this.input.mousePointer.y)
+      // console.log(Ball.x, Ball.y)
+    }
   }
 }
