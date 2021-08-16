47588e89256fcf1b9834b398b75799842717194a
vhg249 <vhuongptit@gmail.com>
2021-08-16 21:51:37 +0700

    add minimap demo

--- src/config.js
+++ src/config.js
@@ -6,5 +6,14 @@ const height = docElement.clientHeight > '100%' ? '100%' : docElement.clientHeig
 export default {
   width: width,
   height: height,
-  type: Phaser.AUTO
+  type: Phaser.AUTO,
+  physics: {
+    default: 'matter',
+    matter: {
+        gravity: {
+            x: 0,
+            y: 0
+        },
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Phaser from 'phaser'
 import config from './config'
 import Boot from './states/Boot'
+import Minimap from './scene/Minimap'
 
 const game = new Phaser.Game(config)
 
-game.scene.add('boot', Boot)
-game.scene.start('boot')
+// game.scene.add('boot', Boot)
+// game.scene.start('boot')
+
+console.log('mini');
+game.scene.add('minimap', Minimap)
+game.scene.start('minimap')
