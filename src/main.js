c4b7cb7bd73044351c1aa4ddfc963d66a266fcaf
vhg249 <vhuongptit@gmail.com>
2021-08-21 15:42:42 +0700

    add login form

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Phaser from 'phaser'
 import config from './config'
 import Boot from './states/Boot'
+import Login from './states/Login'
 
 const game = new Phaser.Game(config)
 
 game.scene.add('boot', Boot)
-game.scene.start('boot')
+game.scene.add('login', Login)
+game.scene.start('login')
