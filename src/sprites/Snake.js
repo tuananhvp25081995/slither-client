7be09bfe8040a9fb3803995f86d77ca1155c8903
Duong Bon Jones <iamtdthewinner@gmail.com>
2021-09-07 10:15:38 +0700

    tweens

--- src/sprites/Snake.js
+++ src/sprites/Snake.js
@@ -1,72 +1,75 @@
-import Phaser, { Color } from 'phaser'
+import Phaser, {
+  Color
+} from 'phaser';
 // import EyePair from "./EyePair";
 // import Shadow from "./Shadow";
-import { Util } from '../utils'
-let nickName
+import {
+  Util
+} from '../utils';
+let nickName;
 export default class extends Phaser.GameObjects.Sprite {
   constructor (scene, x, y, texture) {
-    super(scene, x, y, texture)
+    super(scene, x, y, texture);
     // this.anchor.setTo(0.5)
 
     if (!this.scene.game.snakes) {
-      this.scene.game.snakes = []
+      this.scene.game.snakes = [];
     }
 
-    this.scene.game.snakes.push(this)
-    this.debug = false
-    this.snakeLength = 0
-    this.spriteKey = texture
+    this.scene.game.snakes.push(this);
+    this.debug = false;
+    this.snakeLength = 0;
+    this.spriteKey = texture;
 
     // various quantities
-    this.scale = 0.6
-    this.fastSpeed = 200
-    this.slowSpeed = 130
-    this.speed = this.slowSpeed
-    this.rotationSpeed = 40
+    this.scale = 0.6;
+    this.fastSpeed = 200;
+    this.slowSpeed = 130;
+    this.speed = this.slowSpeed;
+    this.rotationSpeed = 40;
 
     // init
     // this.collisionGroup = this.scene.physics.p2.createCollisionGroup()
-    this.sections = []
-    this.headPath = []
-    this.tweens = []
-    this.food = []
-    this.preferredDistance = 17 * this.scale
-    this.queuedSections = 0
+    this.sections = [];
+    this.headPath = [];
+    this.tweens = [];
+    this.food = [];
+    this.preferredDistance = 17 * this.scale;
+    this.queuedSections = 0;
 
-    this.dotSnake = this.scene.add.circle(10, 10, 100, 0xffffff)
-    this.scene.cameras.main.ignore(this.dotSnake)
+    this.dotSnake = this.scene.add.circle(10, 10, 100, 0xffffff);
+    this.scene.cameras.main.ignore(this.dotSnake);
 
     // init shadow
     // this.shadow = new Shadow(this.scene, this.sections, this.scale);
 
     // this.sectionGroup = this.scene.physics.add.group()
-    this.sectionGroup = this.scene.add.container(0, 0)
+    this.sectionGroup = this.scene.add.container(0, 0);
     // add nick name
-    nickName = localStorage.getItem('username')
+    nickName = localStorage.getItem('username');
     this.textPosition = this.scene.add.text(x, y, nickName, {
       fontFamily: 'sans-serif',
       fontSize: '20px',
       color: '#fff'
-    })
+    });
     // add nickname
     // this.sectionGroup.setSize(300, 300)
-    this.scene.physics.world.enable(this.sectionGroup)
-    this.sectionGroup.body.setCollideWorldBounds(true)
+    this.scene.physics.world.enable(this.sectionGroup);
+    this.sectionGroup.body.setCollideWorldBounds(true);
     // this.scene.physics.add.existing(this.sectionGroup);
 
     // add the head of the snake
-    this.head = this.addSectionAtPosition(x, y)
-    this.head.name = 'head'
-    this.head.snake = this
+    this.head = this.addSectionAtPosition(x, y);
+    this.head.name = 'head';
+    this.head.snake = this;
 
     this.lastHeadPosition = new Phaser.Geom.Point(
       this.head.body.x,
       this.head.body.y
-    )
+    );
 
     // init eyes
     // this.eyes = new EyePair(this.scene, this.head, this.scale);
-
 
     // this.sections.forEach((sec) => {
     //   const tween = this.scene.tweens.add({
@@ -74,26 +77,26 @@ export default class extends Phaser.GameObjects.Sprite {
     //     x,
     //     y,
     //     ease: 'Linear',
-    //     duration: 1000
-    //   })
-    //   this.tweens.push(tween)
-    // })
+    //     duration: 3000
+    //   });
+    //   this.tweens.push(tween);
+    // });
 
-    this.onDestroyedCallbacks = []
-    this.onDestroyedContexts = []
+    this.onDestroyedCallbacks = [];
+    this.onDestroyedContexts = [];
 
     // the edge is the front body that can collide with other snakes
     // it is locked to the head of this snake
-    this.edgeOffset = 3
+    this.edgeOffset = 3;
     this.edge = this.scene.physics.add.sprite(
       x,
       y - this.edgeOffset,
       this.spriteKey
-    )
-    this.edge.name = 'edge'
-    this.edge.alpha = 0
+    );
+    this.edge.name = 'edge';
+    this.edge.alpha = 0;
     // this.scene.physics.p2.enable(this.edge, this.debug)
-    this.edge.body.setCircle(this.edgeOffset)
+    this.edge.body.setCircle(this.edgeOffset);
     // constrain edge to the front of the head
     // this.edgeLock = this.scene.physics.p2.createLockConstraint(
     //     this.edge.body,
@@ -108,68 +111,68 @@ export default class extends Phaser.GameObjects.Sprite {
   edgeContact (phaserBody) {
     // if the edge hits another snake's section, destroy this snake
     if (phaserBody && this.sections.indexOf(phaserBody.sprite) === -1) {
-      this.destroy()
+      this.destroy();
     } else if (phaserBody) {
       // if the edge hits this snake's own section, a simple solution to avoid
       // glitches is to move the edge to the center of the head, where it
       // will then move back to the front because of the lock constraint
-      this.edge.body.x = this.head.body.x
-      this.edge.body.y = this.head.body.y
+      this.edge.body.x = this.head.body.x;
+      this.edge.body.y = this.head.body.y;
     }
   }
 
   addSectionAtPosition (x, y) {
     // init new section
-    const sec = this.scene.physics.add.sprite(x, y, this.spriteKey)
+    const sec = this.scene.physics.add.sprite(x, y, this.spriteKey);
 
-    this.snakeLength++
-    this.sectionGroup.add(sec)
-    this.sectionGroup.sendToBack(sec)
-    sec.setScale(this.scale)
-    this.sections.push(sec)
+    this.snakeLength++;
+    this.sectionGroup.add(sec);
+    this.sectionGroup.sendToBack(sec);
+    sec.setScale(this.scale);
+    this.sections.push(sec);
 
     // add circle
     // sec.body.clearShapes();
-    sec.body.setCircle(sec.width * 0.5)
+    sec.body.setCircle(sec.width * 0.5);
 
     const tween = this.scene.tweens.add({
-      targets: this.scene,
-      x: sec.x,
-      y: sec.y,
+      targets: sec,
+      x: x,
+      y: y,
       ease: 'Linear',
       duration: 1000
-    })
-    this.tweens.push(tween)
+    });
+    this.tweens.push(tween);
     // add shadow
     // this.shadow.add(x, y);
-    return sec
+    return sec;
   }
 
   initSections (snakeSections = []) {
     snakeSections.forEach((section) => {
-      this.addSectionAtPosition(section.x, section.y)
-      this.headPath.push(new Phaser.Geom.Point(section.x, section.y))
-    })
+      this.addSectionAtPosition(section.x, section.y);
+      this.headPath.push(new Phaser.Geom.Point(section.x, section.y));
+    });
   }
 
   addSectionsAfterLast (amount) {
-    this.queuedSections += amount
+    this.queuedSections += amount;
   }
 
   update (snakeDataUpdate) {
     if (!snakeDataUpdate || !Array.isArray(snakeDataUpdate.circleSnake)) {
-      return
+      return;
     }
-    const snakeSections = [...snakeDataUpdate.circleSnake]
+    const snakeSections = [...snakeDataUpdate.circleSnake];
     for (let i = 0; i < snakeSections.length; i++) {
-      this.sections[i].body.x = snakeSections[i].x
-      this.sections[i].body.y = snakeSections[i].y
+      // this.sections[i].body.x = snakeSections[i].x;
+      // this.sections[i].body.y = snakeSections[i].y;
 
-      // this.tweens[i].play()
-      // if (this.tweens[i].isPlaying() && snakeSections[i]) {
-      //   this.tweens[i].updateTo('x', snakeSections[i].x, true)
-      //   this.tweens[i].updateTo('y', snakeSections[i].y, true)
-      // }
+      this.tweens[i].play();
+      if (this.tweens[i].isPlaying() && snakeSections[i]) {
+        this.tweens[i].updateTo('x', snakeSections[i].x, true);
+        this.tweens[i].updateTo('y', snakeSections[i].y, true);
+      }
     }
   }
 
@@ -179,20 +182,20 @@ export default class extends Phaser.GameObjects.Sprite {
      */
   onCycleComplete () {
     if (this.queuedSections > 0) {
-      const lastSec = this.sections[this.sections.length - 1]
-      this.addSectionAtPosition(lastSec.body.x, lastSec.body.y)
-      this.queuedSections--
+      const lastSec = this.sections[this.sections.length - 1];
+      this.addSectionAtPosition(lastSec.body.x, lastSec.body.y);
+      this.queuedSections--;
     }
   }
 
   setScale (scale) {
-    this.scale = scale
-    this.preferredDistance = 17 * this.scale
+    this.scale = scale;
+    this.preferredDistance = 17 * this.scale;
 
     // scale sections and their bodies
     for (let i = 0; i < this.sections.length; i++) {
-      const sec = this.sections[i]
-      sec.scale.setTo(this.scale)
+      const sec = this.sections[i];
+      sec.scale.setTo(this.scale);
       // sec.body.data.shapes[0].radius = this.scene.physics.p2.pxm(sec.width * 0.5)
     }
   }
@@ -205,20 +208,20 @@ export default class extends Phaser.GameObjects.Sprite {
   // this.eyes.setScale(scale);
 
   incrementSize () {
-    this.addSectionsAfterLast(1)
-    this.setScale(this.scale * 1.01)
+    this.addSectionsAfterLast(1);
+    this.setScale(this.scale * 1.01);
   }
 
   addDestroyedCallback (callback, context) {
-    this.onDestroyedCallbacks.push(callback)
-    this.onDestroyedContexts.push(context)
+    this.onDestroyedCallbacks.push(callback);
+    this.onDestroyedContexts.push(context);
   }
 
   destroy () {
-    this.scene.snakes.splice(this.scene.snakes.indexOf(this), 1)
+    this.scene.snakes.splice(this.scene.snakes.indexOf(this), 1);
     this.sections.forEach(function (sec, index) {
-      sec.destroy()
-    })
+      sec.destroy();
+    });
 
     // call this snake's destruction callbacks
     for (let i = 0; i < this.onDestroyedCallbacks.length; i++) {
@@ -226,11 +229,11 @@ export default class extends Phaser.GameObjects.Sprite {
         this.onDestroyedCallbacks[i].apply(
           this.onDestroyedContexts[i],
           [this]
-        )
+        );
       }
     }
     // this.scene.physics.p2.removeConstraint(this.edgeLock)
-    this.edge.destroy()
+    this.edge.destroy();
     // this.eyes.destroy();
     // this.shadow.destroy();
   }
