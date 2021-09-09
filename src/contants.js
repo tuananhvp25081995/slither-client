126655eba7bc70e25ee4786a906ecd8167c7b8bb
Duong Bon Jones <iamtdthewinner@gmail.com>
2021-09-09 20:13:06 +0700

    init skill events

--- src/contants.js
+++ src/contants.js
@@ -5,6 +5,12 @@ module.exports = {
     PLAYERSENDTARGET: 'change_target',
     SERVER_TELL_PLAYER_TO_MOVE: 'game:self-snakes-received',
     SERVER_UPDATE_ALL_PLAYERS: 'game:snakes-received',
-    SERVER_UPDATE_FOOD: 'game:foods-received'
+    SERVER_UPDATE_FOOD: 'game:foods-received',
+    USE_SKILL: 'use_skill',
+    SERVER_SKILL_THROUGH: 'game:snake-through',
+    SERVER_SKILL_INVISIBLE: 'game:snake-invisible',
+    SERVER_SKILL_SUCK: 'game:snake-suck',
+    SERVER_SKILL_FEED: 'game:snake-feed',
+    SERVER_SKILL_ZOOM: 'game:snake-zoom'
   }
 };
