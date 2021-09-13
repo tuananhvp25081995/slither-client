055c497d4a4652a3257fc77249e3e066d766d62a
Loc Nguyen <nguyentongloc1996@gmail.com>
2021-09-13 15:36:37 +0700

    reformat

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Phaser from 'phaser'
-import config from './config'
-import Boot from './states/Boot'
-import Game from './states/Game'
-import Login from './states/Login'
-import Countdown from './states/Countdown'
+import Phaser from 'phaser';
+import config from './config';
+import Boot from './states/Boot';
+import Game from './states/Game';
+import Login from './states/Login';
+import Countdown from './states/Countdown';
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
 
-game.scene.add('boot', Boot)
-game.scene.add('login', Login)
-game.scene.add('countdown', Countdown)
-game.scene.add('game', Game)
-game.scene.start('boot')
+game.scene.add('boot', Boot);
+game.scene.add('login', Login);
+game.scene.add('countdown', Countdown);
+game.scene.add('game', Game);
+game.scene.start('boot');
